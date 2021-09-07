6155af961a7f4a27eed7d147dfa9d011355da914
Margarita555 <a_rita@ukr.net>
2021-09-07 11:24:38 +0300

    +

--- src/index.js
+++ src/index.js
@@ -3,15 +3,6 @@ import menuCardTemplate from '../src/templates/menu-card.hbs';
 
 const menuContainer = document.querySelector('.js-menu');
 const themeSwitch = document.querySelector('.theme-switch__toggle');
-// const Theme = {
-//   LIGHT: 'light-theme',
-//   DARK: 'dark-theme',
-// };
-
-// function createMenuCardsMarkup(menu) {
-//     return menu.map(menuCardTemplate).join('');
-// }
-// const cardMarkup = createMenuCardsMarkup(menu);
 
 const cardMarkup = menuCardTemplate(menu);
  menuContainer.insertAdjacentHTML('beforeend', cardMarkup);
@@ -20,7 +11,6 @@ const cardMarkup = menuCardTemplate(menu);
 document.addEventListener('DOMContentLoaded', setTheme);
 function setTheme() {
     let theme = localStorage.getItem('theme');
-    // console.log(theme)
     if(theme == 'dark'){
         document.body.classList.add('dark-theme');
         themeSwitch.checked = true;
@@ -28,8 +18,6 @@ function setTheme() {
         document.body.classList.add('light-theme');
     }
 }
-
-
 
 themeSwitch.addEventListener('change', onThemeSwitchChange);
 function onThemeSwitchChange() {
@@ -46,13 +34,11 @@ function onThemeSwitchChange() {
 }
 
 // Добавь функционал изменения темы при нажатии (событие change) на чекбокс #theme-switch-toggle в тулбаре.
-
 // По умолчанию тема светлая.
 // При изменении темы, необходимо добавлять на элемент body класс light-theme или dark-theme.
 // Выбранная тема должна сохраняться между перезагрузками страницы. Для хранения темы используй localStorage.
 // Если при загрузке страницы тема тёмная, не забудь поставить свойство checked у чекбокса #theme-switch-toggle в true, чтобы ползунок сдвинулся в правильное положение.
 // Для удобства хранения списка тем используй такое перечисление.
-
 // const Theme = {
 //   LIGHT: 'light-theme',
 //   DARK: 'dark-theme',
