afaa21d4e655243a454be2b0f0ef4429d0b378aa
Margarita555 <a_rita@ukr.net>
2021-09-09 09:53:56 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,20 +12,17 @@ document.addEventListener('DOMContentLoaded', setTheme);
 function setTheme() {
     let theme = localStorage.getItem('theme');
     if(theme == 'dark'){
-        document.body.classList.add('dark-theme');
+        document.body.classList.replace('light-theme', 'dark-theme');
         themeSwitch.checked = true;
-    }if(theme == 'light'){
-        document.body.classList.add('light-theme');
     }
 }
 
 themeSwitch.addEventListener('change', onThemeSwitchChange);
 function onThemeSwitchChange() {
-    //  document.body.classList.add('dark-theme');
     if(document.body.classList.contains('light-theme')){
         document.body.classList.replace('light-theme', 'dark-theme');
         localStorage.setItem('theme', 'dark');
-       return;
+        return;
     } if (document.body.classList.contains('dark-theme')) {
         document.body.classList.replace('dark-theme', 'light-theme');
         localStorage.setItem('theme', 'light');
@@ -33,19 +30,6 @@ function onThemeSwitchChange() {
     }
 }
 
-// themeSwitch.addEventListener('change', onThemeSwitchChange);
-// function onThemeSwitchChange() {
-
-//     if(document.body.classList.contains('light-theme')){
-//         document.body.classList.replace('light-theme', 'dark-theme');
-//         localStorage.setItem('theme', 'dark');
-//        return;
-//     } if (document.body.classList.contains('dark-theme')) {
-//         document.body.classList.replace('dark-theme', 'light-theme');
-//         localStorage.setItem('theme', 'light');
-//         return; 
-//     }
-// }
 
 // Добавь функционал изменения темы при нажатии (событие change) на чекбокс #theme-switch-toggle в тулбаре.
 // По умолчанию тема светлая.
@@ -59,5 +43,4 @@ function onThemeSwitchChange() {
 // };
 // Шаблонизация
 // Используя шаблонизатор Handlebars создай шаблон одного элемента меню. После чего, используя шаблон, создай разметку всего меню по данным из menu.json и добавь в DOM в ul.js-menu.
-
 // Для иконок используется библиотека Material Icons, линк на веб-фонт уже есть в исходном HTML.
